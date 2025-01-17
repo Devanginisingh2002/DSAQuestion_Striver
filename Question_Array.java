b24a78608e973ec6e38ae268bfb9c4906f00f7f1
Devanginisingh2002 <devangini123raghuvanshi@gmail.com>
2025-01-17 23:37:03 +0530

    Question

--- Question_Array.java
+++ Question_Array.java
@@ -33,28 +33,63 @@ public class Question_Array {
   // Brute force approach: O(NlogN + N)
 
   // Better Approach: O(N+N) -> O(2N)
-  public static int largest(int arr[]) {
-    int largest = arr[0];
-    for (int i = 0; i < arr.length; i++) {
-      if (arr[i] > largest) {
-        largest = arr[i];
-      }
-    }
-    return largest;
-  }
-  public static int secondLargest(int arr[]) {
-    int largest = largest(arr); // call largest method
+  //   public static int largest(int arr[]) {
+  //     int largest = arr[0];
+  //     for (int i = 0; i < arr.length; i++) {
+  //       if (arr[i] > largest) {
+  //         largest = arr[i];
+  //       }
+  //     }
+  //     return largest;
+  //   }
+  //   public static int secondLargest(int arr[]) {
+  //     int largest = largest(arr); // call largest method
+  //     int second_largest = Integer.MIN_VALUE;
+  //     for (int i = 0; i < arr.length; i++) {
+  //       if (arr[i] > second_largest && arr[i] != largest) {
+  //         second_largest = arr[i];
+  //       }
+  //     }
+  //     return second_largest;
+  //   }
+
+  //   public static void main(String args[]) {
+  //     int arr[] = {1, 2, 4, 7, 7, 5};
+  //     System.out.println(secondLargest(arr));
+  //   }
+  // optimize approach: T.C: O(n)
+  public static int second_largest(int nums[]) {
+    int largest = nums[0];
     int second_largest = Integer.MIN_VALUE;
-    for (int i = 0; i < arr.length; i++) {
-      if (arr[i] > second_largest && arr[i] != largest) {
-        second_largest = arr[i];
-      }
+    for(int i=1; i<nums.length; i++){
+        if(nums[i] > largest){
+            second_largest = largest;
+            largest = nums[i];
+        }
+        else if(nums[i] < largest && nums[i] > second_largest){
+            second_largest = nums[i];
+        }
     }
     return second_largest;
   }
+  public static int second_smallest(int nums[]){
+    int smallest = nums[0];
+    int second_smallest = Integer.MAX_VALUE;
+    for(int i=1; i<nums.length; i++){
+        if(nums[i] < smallest){
+            second_smallest = smallest;
+            smallest = nums[i];
+        }
+        else if(nums[i] > smallest && nums[i] < second_smallest){
+            second_smallest = nums[i];
+        }
+    }
+    return second_smallest;
+  }
 
-  public static void main(String args[]) {
-    int arr[] = {1, 2, 4, 7, 7, 5};
-    System.out.println(secondLargest(arr));
+  public static void main(String args[]){
+    int nums[] = {2, 5, 6, 7 ,1};
+    System.out.println(second_largest(nums));
+    System.out.println(second_smallest(nums));
   }
 }
